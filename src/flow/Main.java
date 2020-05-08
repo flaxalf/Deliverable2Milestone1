ec7d977194501e54403377d1073800c726b20b6f
flaxalf <37453989+flaxalf@users.noreply.github.com>
2020-05-08 12:37:48 +0200

    added project

---
+++ src/flow/Main.java
@@ -0,0 +1,11 @@
+package flow;
+
+import java.util.logging.Logger;
+
+public class Main {
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+	public static void main(String[] args) {
+		LOGGER.info("Hello world");
+	}
+}
